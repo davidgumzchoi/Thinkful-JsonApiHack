980a780e6eaae32959f1044b54dafd4d4d0aabe9
dchoi <dchoi@switchfly.com>
2015-02-19 05:57:16 +0000

    needs styling

--- js/application.js
+++ js/application.js
@@ -35,50 +35,51 @@ list, check this out (http://www.programmableweb.com/apis/directory/1?format=JSO
 */
 
 var access_token = "12228466.31599a5.7e5fd8ae83094826b85eacfaad7dd185",
-	hashtag = "ficb2010"
-	hashtag_url = "https://api.instagram.com/v1/tags/"+hashtag+"/media/recent?access_token="+access_token;
+	resolution = "thumbnail",
+	user_id = "12228466",
+	hashtag = "ficb2010",
+	last_url = "",
+	// HASHTAG URL - USE THIS URL FOR HASHTAG PICS
+	hashtag_url = "https://api.instagram.com/v1/tags/"+hashtag+"/media/recent?access_token="+access_token,
+	// USER URL - USE THIS URL FOR USER PICS
+	user_url = "https://api.instagram.com/v1/users/"+user_id+"/media/recent/?access_token="+access_token;
 
-function loadEmUp(next_url) {
-
+function loadFeed(next_url){
 	url = next_url;
-	
 	$(function() {
-		$.ajax({
-			type: "GET",
-			dataType: "jsonp",
-			cache: false,
-			url: url,
-			success: function(data) {
-				next_url = data.pagination.next_url;
-		  		count = 20; 
+	    $.ajax({
+	    	type: "GET",
+	        dataType: "jsonp",
+	        cache: false,
+	        url: url,
+	        
+	        success: function(data) {
+		  		next_url = data.pagination.next_url;
+		  		count = 20;
+	            for (var i = 0; i < count; i++) {
+						if (typeof data.data[i] !== 'undefined') {
+						//console.log("id: " + data.data[i].id);
+							$("#instagram").append("<div class='instagram-wrap' id='pic-" + data.data[i].id + "'><a target='_blank' href='" + data.data[i].link + "'><span class='likes'>" + data.data[i].likes.count + "</span><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>"
+						);
+					}
+	      		}
 
-		  		//uncommment to see da codez
-		        //console.log("count: " + count );
-		        //console.log("next_url: " + next_url );
-				//console.log("data: " + JSON.stringify(data) );
-				
-				for (var i = 0; i < count; i++) {
-					if (typeof data.data[i] !== 'undefined') {
-						$("#instagram").append("<div class='instagram-wrap' id='pic-"+ data.data[i].id +"'><a target='_blank' href='" + data.data[i].link +"'><span class='likes'>"+data.data[i].likes.count +"</span><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");  
-					}  
-				}     
-
-				console.log("next_url: " + next_url);
-				$("#showMore").hide();
-				if (typeof next_url === 'undefined' || next_url.length < 10 ) {
-					console.log("NO MORE");
-					$("#showMore").hide();
-					$("#more").attr( "next_url", "");
-				} else {
-		        //set button value
+		  		console.log("next_url: " + next_url);
+		  		$("#showMore").hide();
+		  		if (typeof next_url === 'undefined' || next_url.length < 10 ) {
+			  		console.log("NO MORE");
+			  		$("#showMore").hide();
+			  		$("#more").attr("next_url", "");
+		  		} else {
+			        //set button value
 			        console.log("displaying more");
 			        $("#showMore").show();
-			        $( "#more" ).attr( "next_url", next_url);
-			        last_url = next_url;
-		    	}
-			}
-		});
-	}
+			        $("#more").attr("next_url", next_url);
+			        last_url = next_url;		      		
+	      		}
+	        }
+	    });
+	});
 }
 
 //CALL THE SCRIPT TO START...
@@ -86,8 +87,9 @@ $( document ).ready(function() {
 	//APPEND LOAD MORE BUTTON TO THE BODY...
 	$("#more" ).click(function() {  
 		var next_url = $(this).attr('next_url');
-		loadEmUp(next_url);
+		loadFeed(next_url);
 		return false;
 	});
-	loadEmUp(hashtag_url);
+	//start your engines
+	loadFeed(hashtag_url);
 });
